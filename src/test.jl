10e5cc552f915f6aacb58e0a137291ab421faa85
chrhenr <chrhenr@chalmers.se>
2025-05-28 20:26:25 +0200

    changed bool to i1

--- src/test.jl
+++ src/test.jl
@@ -1,11 +1,23 @@
 int main() {
-    int[] xs = new int[5];
+  boolean[] bools = new boolean[10];
+  double[] doubles = new double[10];
+  int j=0;
+  double x = 0.0;
+  while (j<bools.length) {
+    doubles[j] = x*2.5;
+    bools[j] = doubles[j] > 15.0;
+    j++;
+    x = x + 1.0;
+  }
 
-    xs[2]--;
-    xs[1]++;
-
-    for(int x : xs)
-        printInt(x);
-
-    return 0;
+  j = 0;
+  for(boolean b : bools) {
+    if(b) {
+      printString("yes");
+    } else {
+      printDouble(doubles[j]);
+    }
+    j++;
+  }
+  return 0;
 }
